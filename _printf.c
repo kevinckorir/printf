ada3814942a50306fcdb3f3cbd93bf925eaa1f8d
Kevin Korir <korirckevin@gmail.com>
2023-04-28 15:30:34 +0300

    made betty compliant

--- _printf.c
+++ _printf.c
@@ -3,55 +3,70 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * _printf - prints next line in buffer
+ * @format: checks format
+ * Return: number of characters
+ */
+
 int _printf(const char *format, ...)
 {
-    va_list arg;
-    int done = 0;
+	va_list arg;
+	int done = 0;
 
-    va_start(arg, format);
+	va_start(arg, format);
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c': {
-                    int c = va_arg(arg, int);
-                    write(1, &c, 1);
-                    done++;
-                    break;
-                }
-                case 's': {
-                    char *s = va_arg(arg, char*);
-                    size_t len = strlen(s);
-                    write(1, s, len);
-                    done += len;
-                    break;
-                }
-                case '%': {
-                    char c = '%';
-                    write(1, &c, 1);
-                    done++;
-                    break;
-                }
-                default: {
-                    char *str = (char*) malloc(2 * sizeof(char));
-                    str[0] = '%';
-                    str[1] = *format;
-                    write(1, str, 2);
-                    done += 2;
-                    free(str);
-                    break;
-                }
-            }
-        } else {
-            write(1, format, 1);
-            done++;
-        }
-        format++;
-    }
+	while (*format != '\0')
+	{
+	if (*format == '%')
+	{
+	format++;
+		switch (*format)
+		{
+			case 'c':
+			{
+			int c = va_arg(arg, int);
 
-    va_end(arg);
+			write(1, &c, 1);
+			done++;
+			break;
+			}
+			case 's':
+			{
+			char *s = va_arg(arg, char*);
+			size_t len = strlen(s);
+			write(1, s, len);
+			done += len;
+			break;
+			}
+			case '%': 
+			{
+			char c = '%';
+			write(1, &c, 1);
+			done++;
+			break;
+			}
+			default: {
+			char *str = (char*) malloc(2 * sizeof(char));
+			str[0] = '%';
+			str[1] = *format;
+			write(1, str, 2);
+			done += 2;
+			free(str);
+			break;
+			}
+		}
+	}
+       	else
+	{
+		write(1, format, 1);
+		done++;
+	}
+	format++;
+	}
 
-    return done;
+	va_end(arg);
+
+	return (done);
 }
 
